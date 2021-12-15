7a131635887777ff94094c70fcacc4929e89ffa8
gordiic <nebojsagordic@gmail.com>
2021-12-15 21:23:15 +0100

    frontend commit 3

--- frontend/src/OnlineAccount.js
+++ frontend/src/OnlineAccount.js
@@ -19,6 +19,7 @@ import {
   StatLabel,
   StatNumber,
   StatHelpText,
+  Select,
 } from "@chakra-ui/react";
 import {
   EmailIcon,
@@ -36,8 +37,14 @@ const OnlineAccount = () => {
   const [person, setPerson] = useState(
     JSON.parse(localStorage.getItem("user"))
   );
-  const [currencyOptions, setCurrencyOption] = useState([]);
-  const [payment, setPayment] = useState({ amount: 0 });
+  const [payment, setPayment] = useState({
+    amount: "",
+    rsdBalance: "",
+    email: "",
+  });
+
+  const [array, setArray] = useState([]);
+  const [currency, setCurrency] = useState([]);
   const [error, setError] = useState("");
   const [creditCard, setcreditCard] = useState({
     number: "",
@@ -49,13 +56,20 @@ const OnlineAccount = () => {
 
   useEffect(() => {
     setPerson(JSON.parse(localStorage.getItem("user")));
-    // fetch(URL)
-    //   .then((res) => res.json())
-    //   .then((data) =>
-    //     setCurrencyOption([data.base, Object.keys.apply(data.rates)])
-    //   );
+    fetch(URL)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        //setBaseCurrency(data.base);
+        setCurrency(data.rates);
+        setArray([data.base, ...Object.keys(data.rates)]);
+      });
   }, []);
 
+  useEffect(() => {
+    var rsdbal = person.onlineAccount.balances["RSD"];
+    setPayment({ amount: "", rsdBalance: rsdbal, email: person.email });
+  }, [person]);
   const handleChange = (e) => {
     const name = e.target.name; //atribut u tagu, da li je to email, godine ili ime
     const value = e.target.value;
@@ -90,22 +104,24 @@ const OnlineAccount = () => {
   };
   const handleSubmit2 = (e) => {
     e.preventDefault();
+
     console.log(payment);
     if (payment.amount) {
       if (payment.amount >= 1) {
-        depositOnAccount(payment)
-          .then((item) => {
-            if (item !== "Deposit failed.") {
-              window.localStorage.setItem("user", JSON.stringify(item));
-              setError("");
-            } else {
-              setError(item);
-            }
-          })
-          .then(() => {
-            setPerson(window.localStorage.getItem("user"));
-            window.location.reload();
-          });
+        console.log(array);
+        // depositOnAccount(payment)
+        //   .then((item) => {
+        //     if (item !== "Deposit failed.") {
+        //       window.localStorage.setItem("user", JSON.stringify(item));
+        //       setError("");
+        //     } else {
+        //       setError(item);
+        //     }
+        //   })
+        //   .then(() => {
+        //     setPerson(window.localStorage.getItem("user"));
+        //     window.location.reload();
+        //   });
       } else {
         setError("Unesite kolicinu za uplatu vecu od 0.");
       }
@@ -125,69 +141,76 @@ const OnlineAccount = () => {
       <Stack direction="row" border={1}>
         <Stack spacing={3}>
           {person && person.verified && (
-            <Stack direction="column">
-              <>
-                <Input
-                  type="number"
-                  name="broj"
-                  id="broj"
-                  value={payment}
-                  onChange={handleChange2}
-                  placeholder="Kolicina"
-                  variant="filled"
-                  textColor="black"
-                />
-                <select>
-                  <option></option>
-                </select>
-              </>
-              <>
-                <Button
-                  textColor="gray.200"
-                  colorScheme="green"
-                  textColor="black"
-                  margin={1}
-                >
-                  Verifikovan
-                </Button>
-                <form action="submit">
-                  <FormControl isRequired>
-                    <Text fontSize="sm">Kolicina za uplatu:</Text>
-                    <InputGroup>
-                      <InputLeftElement children={<Icon as={InfoIcon} />} />
-                      <Input
-                        type="number"
-                        name="amount"
-                        id="amount"
-                        value={payment.amount}
-                        onChange={handleChange2}
-                        placeholder="Kolicina"
-                        variant="filled"
-                        textColor="black"
-                      />
-                      <InputRightAddon children="RSD" />
-                    </InputGroup>
-                  </FormControl>
-                  <br />
-                  <Stat>
-                    <StatLabel>Trenutno stanje on-line racuna</StatLabel>
-                    <StatNumber></StatNumber>
-                    <StatHelpText>RSD</StatHelpText>
-                  </Stat>
-                  <br />
-                  <Divider />
-                  <br />
+            <Stack direction="row">
+              <Stack direction="column">
+                <Select size="xs" placeholder="From:" name="currency">
+                  {array.map((cur) => {
+                    return (
+                      <option key={cur} value={cur}>
+                        {cur}
+                      </option>
+                    );
+                  })}
+                </Select>
+                <Select size="xs" placeholder="To:" name="currency">
+                  {array.map((cur) => {
+                    return (
+                      <option key={cur} value={cur}>
+                        {cur}
+                      </option>
+                    );
+                  })}
+                </Select>
+              </Stack>
+              <Stack direction="column">
+                <>
                   <Button
-                    width="100%"
+                    textColor="gray.200"
+                    colorScheme="green"
                     textColor="black"
-                    colorScheme="yellow"
-                    type="submit"
-                    onClick={handleSubmit2}
+                    margin={1}
                   >
-                    Uplati na racun
+                    Verifikovan
                   </Button>
-                </form>
-              </>
+                  <form action="submit">
+                    <FormControl isRequired>
+                      <Text fontSize="sm">Kolicina za uplatu:</Text>
+                      <InputGroup>
+                        <InputLeftElement children={<Icon as={InfoIcon} />} />
+                        <Input
+                          type="number"
+                          name="amount"
+                          id="amount"
+                          value={payment.amount}
+                          onChange={handleChange2}
+                          placeholder="Kolicina"
+                          variant="filled"
+                          textColor="black"
+                        />
+                        <InputRightAddon children="RSD" />
+                      </InputGroup>
+                    </FormControl>
+                    <br />
+                    <Stat>
+                      <StatLabel>Trenutno stanje on-line racuna</StatLabel>
+                      <StatNumber>{payment.rsdBalance}</StatNumber>
+                      <StatHelpText>RSD</StatHelpText>
+                    </Stat>
+                    <br />
+                    <Divider />
+                    <br />
+                    <Button
+                      width="100%"
+                      textColor="black"
+                      colorScheme="yellow"
+                      type="submit"
+                      onClick={handleSubmit2}
+                    >
+                      Uplati na racun
+                    </Button>
+                  </form>
+                </>
+              </Stack>
             </Stack>
           )}
           {person && !person.verified && (
