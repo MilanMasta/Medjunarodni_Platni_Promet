0aae2b18311f9325b10fa9660260e8939f8405f8
Nikola Orašanin <79918827+nikolaorasanin@users.noreply.github.com>
2021-12-16 17:15:52 +0100

    Korekcije

--- backend/main.py
+++ backend/main.py
@@ -12,16 +12,20 @@ x = []
 creditCard = CreditCard(1, "Max Verstappen", "30.05.2022.", 225883)
 user = User(0, "", "", "", "", "", "", "", "", 1)
 users={}         # dict -> key = id && value = user
-creditCards={creditCard} # dict -> key = creditCardNum && value = creditCard
+creditCards={} # dict -> key = creditCardNum && value = creditCard
+creditCards[creditCard.get_number()] = creditCard
+global usersCount
 usersCount=0
 
 
 
 @app.route("/register", methods=['POST'])
 def register():
+    #trebamo provjeravati da li korisnik sa tim emailom vec postoji!!!!!!!!!!!!!
+    global usersCount
     req = request.json
     print(req)
-    user.set_id(usersCount+1) # setting id with global counter
+    user.set_id(usersCount + 1) # setting id with global counter
     usersCount += 1
     user.set_name(req["name"])
     user.set_lastname(req["lastname"])
@@ -33,30 +37,34 @@ def register():
     user.set_password(req["password"])
     user.set_verified(False)
     
-    users[user.get_email()]=user
+    users[user.get_id()]=user
+    print(users)
     print(user)
     return "Successfully registered."
 
 @app.route("/login", methods=['POST'])
 def login():
     req = request.json
-    if req["email"] in users:  
-        if User(users[req["email"]]).get_password() == req["password"]:
-             return user.userToJSON()
-        else:
-            return "Invalid password."
-    else:
-        return "User doesn't exits."
-  
+    for key in users:
+        print("LOGIN")
+        print(users[key])
+        if users[key].get_email() == req["email"]:
+            if(users[key].get_password() == req["password"]):
+                print(users[key].userToJSON())
+                return users[key].userToJSON()
+            else:
+                return "Invalid password."
+
+    return "User doesn't exist."
 
 
 @app.route("/changeAccount", methods=['POST'])
 def changeAccount():
     req = request.json
     #user.set_id(req["id"])
-    for i in users:
-        if i.id==req["id"]:
-            user=i
+    for key in users:
+        if key==req["id"]:
+            user=users[key]
     user.set_name(req["name"])
     user.set_lastname(req["lastname"])
     user.set_address(req["address"])
@@ -65,28 +73,28 @@ def changeAccount():
     user.set_phoneNumber(req["phoneNumber"])
     user.set_email(req["email"])
     user.set_password(req["password"])
-    for i in users:
-        if i.id==req["id"]:
-            users.remove(i)
-            break;
+    for key in users:
+        if key==req["id"]:
+            users.pop(key)
+            break
     users[req["id"]]=user
+    print(users[req["id"]])
     return "Updated."
 
 @app.route("/accountVerification", methods=['POST'])
 def accountVerification():
     req = request.json
-    if req["number"] in creditCards:
+    if int(req["number"]) in creditCards:
         if (creditCards[int(req["number"])]).get_csc() == int(req["csc"]) : 
-            if (creditCards[int(req["number"])]).get_expirationDate() == int(req["expirationDate"]):
-                if int(req["id"]) in users:
-                    users[int(req["id"])].set_verified(True)
-                else:
-                    return "Id for verification doesn't exists."
-                creditCards[int(req["number"])].withdraw(1)
-                print("Verified.")
-                return user.userToJSON()
+            #provjeriti expirationDate sa danasnjim datumom (dd/yy)
+            if int(req["id"]) in users:
+                users[int(req["id"])].set_verified(True)
             else:
-                return "Invalid expiration date."
+                return "Id for verification doesn't exists."
+            creditCards[int(req["number"])].withdraw(1)
+            users[req["id"]].set_onlineAccountBalance(0,"RSD")
+            print("Verified.")
+            return users[req["id"]].userToJSON()
         else:
             return "Invalid CSC."
     else:
@@ -97,9 +105,6 @@ def depositOnAccount():
     req = request.json
     print(req["amount"])
     print(req["rsdBalance"])
-    #imacemo i email u req -> (req["email"])
-    #ISPRAVKA: trebali bismo da imamo id u req a ne email jer imamo dict u kom je kljuc id usera
-    # stoga-> 
     if int(req["id"]) in users:
         user=users[int(req["id"])]
     pom_creditCard=None
@@ -111,7 +116,7 @@ def depositOnAccount():
         return user.userToJSON()
     else:
         print("DEPOSIT FAILED.")
-        return "Deposition failed." # promenila sam string za return ali ne vidim gde na frontu treba da ga izmenim
+        return "Deposition failed."
 
 
 
