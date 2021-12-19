85a2992b44b4693d7a37f4e8f3cf233c62d82745
Milan Mastilovic <milanmastilovic1@gmail.com>
2021-12-19 19:54:58 +0100

    DataRange ispravka

--- backend/app/db/CreditCard.py
+++ backend/app/db/CreditCard.py
@@ -2,8 +2,8 @@ from backend.app import db
 
 class CreditCard(db.Model):
     number = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    expirationDate = db.Column(db.String(8), nullable=False)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    expirationDate = db.Column(db.String(20), nullable=False)
     csc = db.Column(db.Integer, nullable=False)
     balance = db.Column(db.Integer)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
--- backend/app/db/User.py
+++ backend/app/db/User.py
@@ -4,14 +4,14 @@ from backend.app.db import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20), nullable=False)
-    lastname = db.Column(db.String(20), nullable=False)
-    address = db.Column(db.String(20), nullable=False)
-    city = db.Column(db.String(20), nullable=False)
-    country = db.Column(db.String(20), nullable=False)
-    phoneNumber = db.Column(db.String(15), nullable=False)
-    email = db.Column(db.String(20), unique=True, nullable=False)
-    password = db.Column(db.String(20), nullable=False)
+    name = db.Column(db.String(30), nullable=False)
+    lastname = db.Column(db.String(30), nullable=False)
+    address = db.Column(db.String(50), nullable=False)
+    city = db.Column(db.String(30), nullable=False)
+    country = db.Column(db.String(30), nullable=False)
+    phoneNumber = db.Column(db.String(30), nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
     verified = db.Column(db.Boolean, default=False)
     creditCards = db.relationship('CreditCard', backref='id', lazy=True)
 
